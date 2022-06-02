deab24aaacda5c6028269482d959980ebc3bb997
chughatashb <106698433+chughatashb@users.noreply.github.com>
2022-06-02 09:29:56 +0500

    Update SiDra.py

--- SiDra.py
+++ SiDra.py
@@ -422,7 +422,7 @@ class Main:
 		print(" [4] 2009-10 Cloning")
 		print(" [5] 2011-14 Cloning")
 		print(" [E] Exit Programming\n")
-		 KALEEM =input(" Choose : ")
+		KALEEM =input(" Choose : ")
 		if KALEEM in ["1", "01"]:
 			File()
 		if KALEEM in ["2", "02"]:
@@ -431,7 +431,7 @@ class Main:
 			os.system("python Dump.py")
 		if KALEEM in ["4", "04"]:
 			self.old()
-		if UZAIR in ["5", "05"]:
+		if SIDRA in ["5", "05"]:
 			self.old2()
 			exit()
 		else:
@@ -1261,7 +1261,7 @@ class Main:
 			"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"
 		])
 		sys.stdout.write(
-			"\r [UZAIR ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
+			"\r [KALEEM ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
 		); sys.stdout.flush()
 		for pw in pwx:
 			pw = pw.lower()
